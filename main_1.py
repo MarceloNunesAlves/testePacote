c4d136fbb6f92afc39fff9653f24f9d86cb8110a
Marcelo <marcelo.alves@nava.com.br>
2021-05-06 16:04:47 -0300

    Alteração

--- main_1.py
+++ main_1.py
@@ -1,12 +1,20 @@
 from commons.teste import print_1
 import time
+import sys
 import logging
 
 logger = logging.getLogger('Projeto de testes')
+logger.setLevel(logging.DEBUG)
+
+FORMATTER = logging.Formatter("%(asctime)s — %(name)s — %(levelname)s — %(message)s")
+
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setFormatter(FORMATTER)
+logger.addHandler(console_handler)
 
 if __name__ == '__main__':
-    logger.error('Execução da rotina de print - Projeto 1')
+    logger.info('Execução da rotina de print - Projeto 1')
     print_1()
-    logger.error('Logging...')
+    logger.info('Logging...')
 
     #time.sleep(30)
